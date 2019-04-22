ca131fa4d5107ee8aa519b0fe4b1b2f55f94b319
Brooklin Myers <Brooklinjazz@hotmail.com>
2019-04-22 14:24:23 -0700

    fix visiting test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,6 @@
 describe('Visiting Site', function() {
   it('visits the web page on localhost:3000', function() {
     cy.visit("")
-    cy.url().should('eq', "localhost:3000")
+    cy.url().should('eq', "http://localhost:3000/")
   });
 });
