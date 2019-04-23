6cc6257a65c8dc832786e30ffab9ca828f35b98d
Brooklin Myers <Brooklinjazz@hotmail.com>
2019-04-22 23:04:41 -0700

    connect App to redux and configure SendMessageButton

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,20 +2,54 @@ import React from 'react';
 import { IRootstate } from '../../store/store';
 import { IMessage } from '../../store/sending/reducer';
 import { connect } from 'react-redux';
+import { Dispatch, bindActionCreators } from 'redux';
+import { sendMessage } from '../../store/sending/actions';
 
 interface IStateProps {
-  messages: IMessage[]
+  messages: IMessage[];
 }
 
-
 const mapStateToProps = (state: IRootstate): IStateProps => ({
-    messages: state.sending.messages
-})
+  messages: state.sending.messages
+});
 
-const App = ({messages}: IStateProps) => {
+interface IDispatchProps {
+  sendMessage: typeof sendMessage;
+}
+
+const mapDispatchToProps = (dispatch: Dispatch): IDispatchProps =>
+  bindActionCreators(
+    {
+      sendMessage
+    },
+    dispatch
+  );
+
+const message: IMessage = { to: 'test to', content: 'test content' };
+
+interface IProps extends IDispatchProps, IStateProps {}
+
+const App = ({ messages, sendMessage }: IProps) => {
   return (
-    <div data-testid="App" onClick={() => console.log(messages)}  className="App">Click me to log messages</div>
+    <div data-testid="App" className="App">
+      <button
+        onClick={() => sendMessage({ message })}
+        data-testid="SendMessageButton"
+      >
+        Click me to create a message
+      </button>
+      <ol>
+        {messages.map((message, index) => (
+          <li key={index} data-testid={`Message${index}`}>
+            to: {message.to} content: {message.content}
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 };
 
-export default connect(mapStateToProps, null)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
