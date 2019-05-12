fbf45ce6d53994f964a13cc8c289a5485d232bb7
Brooklin Myers <Brooklinjazz@hotmail.com>
2019-05-11 20:29:37 -0600

    remove redux and configure basic socket sending and recieving messages

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,55 +1,25 @@
-import React from 'react';
-import { IRootstate } from '../../store/store';
-import { IMessage } from '../../store/sending/reducer';
-import { connect } from 'react-redux';
-import { Dispatch, bindActionCreators } from 'redux';
-import { sendMessage } from '../../store/sending/actions';
+import React, { useEffect, useRef } from "react";
+import socketIOClient from "socket.io-client";
 
-interface IStateProps {
-  messages: IMessage[];
-}
+const App = () => {
 
-const mapStateToProps = (state: IRootstate): IStateProps => ({
-  messages: state.sending.messages
-});
-
-interface IDispatchProps {
-  sendMessage: typeof sendMessage;
-}
-
-const mapDispatchToProps = (dispatch: Dispatch): IDispatchProps =>
-  bindActionCreators(
-    {
-      sendMessage
-    },
-    dispatch
-  );
-
-const message: IMessage = { to: 'test to', content: 'test content' };
-
-interface IProps extends IDispatchProps, IStateProps {}
-
-const App = ({ messages, sendMessage }: IProps) => {
+  useEffect(() => {
+    const socket = socketIOClient.connect("http://localhost:4000");
+    socket.on("player message", function(data: { message: string }) {
+      console.log("recieved message:", data.message);
+    });
+  }, []);
+  const sendMessage = () => {
+    const socket = socketIOClient.connect("http://localhost:4000");
+    socket.emit('dm message', { message: 'an example dm message' });
+  };
   return (
     <div data-testid="App" className="App">
-      <button
-        onClick={() => sendMessage({ message })}
-        data-testid="SendMessageButton"
-      >
-        Click me to create a message
+      <button onClick={sendMessage} data-testid="SendMessageButton">
+      Click me to create a message
       </button>
-      <ol>
-        {messages.map((message, index) => (
-          <li key={index} data-testid={`Message${index}`}>
-            to: {message.to} content: {message.content}
-          </li>
-        ))}
-      </ol>
     </div>
   );
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default App;
